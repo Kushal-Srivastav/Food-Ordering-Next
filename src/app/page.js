643478f9cf8fe3eee0bc1b9b29154f990d777a37
Kushal-Srivastav <kushalsrijan17@gmail.com>
2025-04-18 10:36:19 +0530

    updated code

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return ( 
+        <main>
+            <img src = "/logo.png" alt = " A server surrounding"></img>
+            <h1>Welcome to the course</h1>
+            <p> Let us get started</p>
+        </main>
+    )
+}
